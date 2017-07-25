5f1c5ed889d2eebe917ebea7d577f85dab72ccad
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-25 16:31:28 -0400

    added product page UI and hooked up iboutlet

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Justin Lindsay on 7/25/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    
+}
