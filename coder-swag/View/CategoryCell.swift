7c5915f53650ef1e5cd1b59389b56bd9c2598c0f
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-25 13:10:23 -0400

    Built out first view, and connected IBOutlets

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Justin Lindsay on 7/25/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
